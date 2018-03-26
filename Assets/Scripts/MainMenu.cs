d42a86806c95895054f76ce62aeec34fa0c2b1ed
Habib Shaikh <habibshaikhofficial@gmail.com>
2018-03-27 02:47:11 +0530

    initial commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour 
+{
+	public Text scoreText;
+	
+	private void Start()
+	{
+		scoreText.text=PlayerPrefs.GetInt("score").ToString();
+	}
+	
+	public  void ToGame()
+	{
+		SceneManager.LoadScene("Game");
+		
+	}
+}
